37fa80fa757a8ab03210065d99c0ad329e7e037c
黄锦辉 <1162006607@qq.com>
2021-12-21 20:50:40 +0800

    commit_try

---
+++ hellogit.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hellogit.c
+	> Author: Feiger
+	> Mail: 1162006607@qq.com
+	> Created Time: Tue 21 Dec 2021 08:48:42 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    fprintf(stdout, "hello git!");
+    return 0;
+}
